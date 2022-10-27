5d05efccb816f65c34f801fb746574d25975343f
Stefano Consonni <stefanoconsonni@ymail.com>
2022-10-27 18:07:03 +0200

    create Navbar + Home + Store + About

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
+
+// components
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
+
 function App() {
 	return (
-		<div className="App">
-			<h1>App</h1>
-		</div>
+		<>
+			<Navbar />
+			<Container className="mb-4">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/store" element={<Store />} />
+					<Route path="/about" element={<About />} />
+				</Routes>
+			</Container>
+		</>
 	);
 }
 
