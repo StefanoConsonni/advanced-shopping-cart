3284f0921f09cd1773c948380c751b5c9629dac8
Stefano Consonni <stefanoconsonni@ymail.com>
2022-10-29 16:52:46 +0200

    add StoreItem component

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,18 @@
+import { Col, Row } from "react-bootstrap";
+import StoreItem from "../components/StoreItem";
+import storeItems from "../data/items.json";
+
 export default function Store() {
-	return <div>Store</div>;
+	return (
+		<>
+			<div>Store</div>
+			<Row xs={1} md={2} lg={3} className="g-3">
+				{storeItems.map((item) => (
+					<Col key={item.id}>
+						<StoreItem {...item} />
+					</Col>
+				))}
+			</Row>
+		</>
+	);
 }
